d960eaca5da9df31dab61ce66f508b1b15897cc1
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-11-23 16:21:29 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Index from "./index";
+import Index from "./pages/Index";
 import "./index.css"; // optional, if you have global styles
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
