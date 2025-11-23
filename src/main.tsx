3db57a6fe750605d95ca461f2ce32e9ce8b8f6a0
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-11-23 16:02:59 +0100

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Index from "./index";
+import "./index.css"; // optional, if you have global styles
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>
+);
