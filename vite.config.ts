5fd4d271924dcf2e388273d0ea62d8e3d62859f6
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-11-23 16:28:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Important for Vercel deployment
+  base: "/", // Important for Vercel deployment
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // <-- Add this line
+    },
+  },
 });
